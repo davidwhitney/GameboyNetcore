c153d69d464a16550feb5b21563e7cc9af3a67cb
David Whitney <david@davidwhitney.co.uk>
2020-02-29 15:58:08 +0000

    WIP

--- GameboyNetcore.Core.Test.Unit/GameBoyTests.cs
+++ GameboyNetcore.Core.Test.Unit/GameBoyTests.cs
@@ -9,22 +9,15 @@ namespace GameboyNetcore.Core.Test.Unit
         private const string TestRom = "C:\\dev\\GameboyNetcore\\GameboyNetcore.Core.Test.Unit\\dmg_test_prog_ver1.gb.bin";
 
         [SetUp]
-        public void Setup()
-        {
-            _sut = new GameBoy();
-        }
+        public void Setup() => _sut = new GameBoy();
 
         [Test]
-        public void Ctor_LoadsOpCodes()
-        {
-            Assert.That(_sut.OpCodes.Count, Is.Not.Zero);
-        }
+        public void Ctor_LoadsOpCodes() 
+            => Assert.That(_sut.OpCodes.Count, Is.Not.Zero);
 
         [Test]
-        public void Ctor_DefaultsToRomNothing()
-        {
-            Assert.That(_sut.Cartridge, Is.EqualTo(Cartridge.Nothing));
-        }
+        public void Ctor_DefaultsToRomNothing() 
+            => Assert.That(_sut.Cartridge, Is.EqualTo(Cartridge.Nothing));
 
         [TestCase("")]
         [TestCase(" ")]
@@ -41,22 +34,21 @@ namespace GameboyNetcore.Core.Test.Unit
             _sut.Load(TestRom);
 
             Assert.That(_sut.Cartridge, Is.Not.EqualTo(Cartridge.Nothing));
-            Assert.That(_sut.Cartridge.Loaded, Is.True);
         }
     }
 
     [TestFixture]
     public class CartridgeTests
     {
-        private Cartridge _sut;
         private const string TestRom = "C:\\dev\\GameboyNetcore\\GameboyNetcore.Core.Test.Unit\\dmg_test_prog_ver1.gb.bin";
+        private const string TetrisRom = "C:\\dev\\GameboyNetcore\\GameboyNetcore.Core.Test.Unit\\tetris.gb";
         
         [Test]
         public void Load_ParsesTitleFromMetadata()
         {
             var rom = new Cartridge(TestRom);
 
-            Assert.That(rom.Title, Is.EqualTo("foo"));
+            Assert.That(rom.Title, Is.EqualTo(""));
         }
 
         [Test]
@@ -64,7 +56,16 @@ namespace GameboyNetcore.Core.Test.Unit
         {
             var rom = new Cartridge(TestRom);
 
-            Assert.That(rom.NintendoLogo, Is.EqualTo("CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D 00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99 BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E"));
+            Assert.That(rom.NintendoLogo.ToHexString(), Is.EqualTo("CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D 00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99 BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E"));
+        }
+
+        [Test]
+        public void Load_ParsesNintendoLogoFromMetadata_Tetris()
+        {
+            var rom = new Cartridge(TetrisRom);
+
+            Assert.That(rom.Title, Is.EqualTo("TETRIS"));
+            Assert.That(rom.IsSuperGameboy(), Is.False);
         }
 
         [Test]
@@ -74,6 +75,14 @@ namespace GameboyNetcore.Core.Test.Unit
 
             Assert.That(rom.RomBank0.Length, Is.EqualTo(16384));
             Assert.That(rom.SwitchableRomBank.Length, Is.EqualTo(16384));
+        }
+    
+        [Test]
+        public void Load_UnderstandsEntryPoint()
+        {
+            var rom = new Cartridge(TestRom);
+
+            Assert.That(rom.EntryPoint.Length, Is.EqualTo(3));
         }
     }
 }
--- GameboyNetcore.Core/GameBoy.cs
+++ GameboyNetcore.Core/GameBoy.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace GameboyNetcore.Core
@@ -26,6 +26,8 @@ namespace GameboyNetcore.Core
 
     public class Cartridge
     {
+        public ReadOnlyMemory<byte> Bytes { get; }
+
         public Cartridge(string path)
         {
             if (path == null || !File.Exists(path))
@@ -36,43 +38,55 @@ namespace GameboyNetcore.Core
             Bytes = new ReadOnlyMemory<byte>(File.ReadAllBytes(path));
         }
 
-        public ReadOnlyMemory<byte> Bytes { get; }
-
-        public ReadOnlyMemory<byte> RomBank0 => Bytes.Range(0x0000, 0x4000);
+        public ReadOnlyMemory<byte> RomBank0 => Bytes.Slice(0x0000, 0x4000);
         public ReadOnlyMemory<byte> SwitchableRomBank => Bytes.Range(0x4000, 0x8000);
 
-        public ReadOnlyMemory<byte> EntryPoint => Bytes.Range(0x0100, 0x0103);
-        public string NintendoLogo => Bytes.Range(0x0104, 0x0133).ToHexString();
-        public string Title => Bytes.Range(0x0134, 0x0143).ToAscii();
+        public ReadOnlyMemory<byte> EntryPoint => Bytes.Slice(0x0100, 3);
+        public ReadOnlyMemory<byte> NintendoLogo => Bytes.Slice(0x0104, 48);
+        public string Title => Bytes.Slice(0x0134, 15).ToAscii();
         public byte SuperGameboyFlag => Bytes.Single(0x0146);
         public byte CartridgeType => Bytes.Single(0x0147);
+        public byte RomSize => Bytes.Single(0x0148);
+        public byte RamSize => Bytes.Single(0x0149);
+        public byte DestinationCode => Bytes.Single(0x014A);
+        public byte OldLicenseeCode => Bytes.Single(0x014B);
+        public byte MaskRomVersionNumber => Bytes.Single(0x014C);
+
+        public byte HeaderChecksum => Bytes.Single(0x014D);
+        // x=0:FOR i=0134h TO 014Ch:x=x-MEM[i]-1:NEXT
+        // The lower 8 bits of the result must be the same than the value in this entry.
+        // The GAME WON'T WORK if this checksum is incorrect.
 
         public static Cartridge Nothing { get; } = new Cartridge();
         private Cartridge() { }
     }
 
+    // Higher level abstractions
+    public static class CartridgeExtensions
+    {
+        public static bool IsSuperGameboy(this Cartridge src)
+            => src.SuperGameboyFlag.ToHexString() == "03";
+    }
+
     public static class ReadOnlyMemoryExtensions
     {
         public static ReadOnlyMemory<byte> Range(this ReadOnlyMemory<byte> src, int start, int end)
-        {
-            var len = end - start;
-            return src.Slice(start, len);
-        }
+            => src.Slice(start, end - start);
+
+        public static string ToHexString(this byte src)
+            => new[] {src}.ToHexString();
 
         public static string ToHexString(this ReadOnlyMemory<byte> src)
-        {
-            return BitConverter.ToString(src.ToArray()).Replace("-", " ");
-        }
+            => src.ToArray().ToHexString();
+
+        public static string ToHexString(this byte[] src) 
+            => BitConverter.ToString(src).Replace("-", " ");
 
         public static string ToAscii(this ReadOnlyMemory<byte> src)
-        {
-            return BitConverter.ToString(src.ToArray());
-        }
+            => Encoding.ASCII.GetString(src.ToArray()).Replace("\0", "");
 
         public static byte Single(this ReadOnlyMemory<byte> src, int offset)
-        {
-            return src.Slice(offset, 1).Single();
-        }
+            => src.Slice(offset, 1).Single();
 
         public static byte Single(this ReadOnlyMemory<byte> src)
         {
